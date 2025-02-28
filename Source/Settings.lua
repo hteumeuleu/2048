a3367ac223892b7045ace71df51e4e1926895886
Rémi Parmentier <remi@hteumeuleu.fr>
2025-02-28 21:32:46 +0100

    feat: remove sounds from settings

--- Source/Settings.lua
+++ Source/Settings.lua
@@ -19,16 +19,11 @@ function Settings:initFromSave()
 	local data = playdate.datastore.read("settings")
 	if data ~= nil then
 		self.crank = false
-		self.sounds = false
 		if data.crank == true then
 			self.crank = true
 		end
-		if data.sounds == true then
-			self.sounds = true
-		end
 	else
 		self.crank = true
-		self.sounds = true
 	end
 
 end
@@ -44,26 +39,12 @@ end
 
 -- addSystemMenuItems()
 --
--- Returns a boolean to enable/disable sounds.
-function Settings:usesSounds()
-
-	return self.sounds
-
-end
-
--- addSystemMenuItems()
---
 function Settings:addSystemMenuItems()
 
 	local menu = playdate.getSystemMenu()
 
 	local crankMenuItem, error = menu:addCheckmarkMenuItem("Crank", self.crank, function(value)
 		self.crank = value
-		self:save()
-	end)
-
-	local soundsMenuItem, error = menu:addCheckmarkMenuItem("Sounds", self.sounds, function(value)
-		self.sounds = value
 		self:save()
 	end)
 
@@ -75,7 +56,6 @@ function Settings:save()
 
 	local data = {}
 	data.crank = self.crank
-	data.sounds = self.sounds
 	playdate.datastore.write(data, "settings")
 
 end
